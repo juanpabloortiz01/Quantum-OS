028ba5b1cb2d9dd719a9ab364e4e0d123f9abe87
Juan Pablo Ortiz <juanpabloortizmoralez@gmail.com>
2026-03-31 17:55:23 -0500

    nada

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "prisma/config"
 
 export default defineConfig({
   datasource: {
-    url: process.env.DATABASE_URL ?? "postgresql://juan:juanpiz01@postgres_postgres:5432/postgres?schema=public&sslmode=disable",
+    url: process.env.DATABASE_URL ?? "postgresql://dummy:dummy@dummy:5432/dummy?schema=public",
   },
 })
