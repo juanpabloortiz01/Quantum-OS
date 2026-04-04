af7624a8fe61bb9d1bcea1c0cc21342470410723
Juan Pablo Ortiz <juanpabloortizmoralez@gmail.com>
2026-04-04 05:00:53 -0500

    fix: prisma 7 client initialization

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "prisma/config"
 
 export default defineConfig({
   datasource: {
-    url: process.env.DATABASE_URL ?? "postgresql://dummy:dummy@dummy:5432/dummy?schema=public",
+    url: process.env.DATABASE_URL!,
   },
 })
